617fde30d8e57937da80bc4a1086d33ea70dd3ea
amartinl89 <andermartin88@gmail.com>
2024-05-09 16:18:49 +0200

    index socket en el html

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -6,14 +6,12 @@ export function setupSockets() {
     socket.on('connect', () => {
         socket.on("desktop-connect", function () {
             console.log("Desktop Connected");
-            // Almacenamos el socket del escritorio
             desktopSocket = socket;
         });
     
         // receives a connect message from a phone
         socket.on("phone-connect", function () {
             console.log("Phone Connected");
-            // Cuando un teléfono se conecta, si hay un socket de escritorio disponible, se le informa al escritorio
             if (desktopSocket) {
                 desktopSocket.emit('phone-connect');
             }
@@ -22,7 +20,6 @@ export function setupSockets() {
         // receives a move message from a phone
         socket.on("phone-move", function (data) {
             console.log("Phone moved: " + data.beta);
-            // Cuando un teléfono se mueve, se lo comunica al socket de escritorio, si existe
             if (desktopSocket) {
                 desktopSocket.emit('phone-move', data.beta);
             }
