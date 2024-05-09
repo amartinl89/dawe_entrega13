4fa7d93a0bc6552f821303d5f878258796aff222
amartinl89 <andermartin88@gmail.com>
2024-05-09 15:53:47 +0200

    export

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,9 +1,9 @@
-const setupSockets = (realtimeListener) => {
+// sockets.js
+
+export function setupSockets(realtimeListener) {
     let desktopSocket = null;
 
     realtimeListener.on('connection', (socket) => {
         // Tu lógica para manejar la conexión de los sockets aquí
     });
 };
-
-module.exports = setupSockets;
