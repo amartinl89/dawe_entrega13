a3b921bf58284bdaa3c606db402522c85f6d9f72
amartinl89 <andermartin88@gmail.com>
2024-05-09 15:57:22 +0200

    socket

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,9 +1,11 @@
 // sockets.js
+import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
 
-export function setupSockets(realtimeListener) {
+export function setupSockets() {
     let desktopSocket = null;
-
-    realtimeListener.on('connection', (socket) => {
-        // Tu lógica para manejar la conexión de los sockets aquí
+    const socket = io.connect('https://dawe2024.me:3000');
+    socket.on('connect', () => {
+        console.log('Connected to server');
+        
     });
 };
