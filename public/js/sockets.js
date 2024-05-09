b4a7a70c22efe64733c9ed5061b5189a00d27af0
amartinl89 <andermartin88@gmail.com>
2024-05-09 16:27:08 +0200

    socket

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -3,28 +3,29 @@ import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
 let desktopSocket = null;
 export function setupSockets() {
     const socket = io.connect('https://dawe2024.me:443');
-    socket.on('connect', () => {
-        socket.on("desktop-connect", function () {
-            console.log("Desktop Connected");
-            desktopSocket = socket;
-        });
+    // socket.on('connect', () => {
+    //     socket.on("desktop-connect", function () {
+    //         console.log("Desktop Connected");
+    //         desktopSocket = socket;
+    //     });
     
-        // receives a connect message from a phone
-        socket.on("phone-connect", function () {
-            console.log("Phone Connected");
-            if (desktopSocket) {
-                desktopSocket.emit('phone-connect');
-            }
-        });
+    //     // receives a connect message from a phone
+    //     socket.on("phone-connect", function () {
+    //         console.log("Phone Connected");
+    //         if (desktopSocket) {
+    //             desktopSocket.emit('phone-connect');
+    //         }
+    //     });
     
-        // receives a move message from a phone
-        socket.on("phone-move", function (data) {
-            console.log("Phone moved: " + data.beta);
-            if (desktopSocket) {
-                desktopSocket.emit('phone-move', data.beta);
-            }
-        });
-    });
+    //     // receives a move message from a phone
+    //     socket.on("phone-move", function (data) {
+    //         console.log("Phone moved: " + data.beta);
+    //         if (desktopSocket) {
+    //             desktopSocket.emit('phone-move', data.beta);
+    //         }
+    //     });
+    // });
+    socket.emit('desktop-connect');
     var update = function(id, value) {
         if (value) {
             value = Math.floor(value);
