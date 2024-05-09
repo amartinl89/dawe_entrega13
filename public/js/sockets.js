5730da68aed6527984621dc4ce7d86944c1acad3
amartinl89 <andermartin88@gmail.com>
2024-05-09 14:04:43 +0000

    se conecta

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -2,10 +2,9 @@
 import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
 
 export function setupSockets() {
-    let desktopSocket = null;
-    const socket = io.connect('https://dawe2024.me:3000');
+    const socket = io.connect('https://dawe2024.me:443');
     socket.on('connect', () => {
-        console.log('Connected to server');
+        socket.emit('Connected to server');
         
     });
 };
