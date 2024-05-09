e35c05729517a2123601a08928782111104d33fa
amartinl89 <andermartin88@gmail.com>
2024-05-09 16:21:08 +0200

    probando mov a socket

--- public/js/main.js
+++ public/js/main.js
@@ -13,32 +13,32 @@ window.onload = () => {
         navigator.vibrate(500);
     });
 
-    var update = function(id, value) {
-        if (value) {
-            value = Math.floor(value);
-            var rotate = 'rotate' + id.toUpperCase() + '(' + (id === 'x' ? -value : value )+ 'deg)';
+    // var update = function(id, value) {
+    //     if (value) {
+    //         value = Math.floor(value);
+    //         var rotate = 'rotate' + id.toUpperCase() + '(' + (id === 'x' ? -value : value )+ 'deg)';
 
-            id = '#' + id;
-            $(id).html(value + '&deg;');
+    //         id = '#' + id;
+    //         $(id).html(value + '&deg;');
 
-            id += '-icon';
-            $(id).css('transform', rotate);
-            $(id).css('-webkit-transform', rotate);
-        }
-    };
+    //         id += '-icon';
+    //         $(id).css('transform', rotate);
+    //         $(id).css('-webkit-transform', rotate);
+    //     }
+    // };
 
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener('deviceorientation', function(e) {
+    // if (window.DeviceOrientationEvent) {
+    //     window.addEventListener('deviceorientation', function(e) {
 
-            socket.emit('phone-move', { alpha: e.alpha, beta: e.beta, gamma: e.gamma});
+    //         socket.emit('phone-move', { alpha: e.alpha, beta: e.beta, gamma: e.gamma});
 
-            $('#frame').text((e.absolute ? 'Earth' : 'arbitrary') + ' coordinates frame');
+    //         $('#frame').text((e.absolute ? 'Earth' : 'arbitrary') + ' coordinates frame');
 
-            update('x', e.beta);
-            update('y', e.gamma);
-            update('z', e.alpha ? 360 - e.alpha : null);
-        });
-    }
+    //         update('x', e.beta);
+    //         update('y', e.gamma);
+    //         update('z', e.alpha ? 360 - e.alpha : null);
+    //     });
+    // }
 }
 };   
 
